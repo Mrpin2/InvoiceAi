2910a7c9e5478834427d619f0b2d67c7847128fc
Mrpin2 <rajeev.bhandari87@gmail.com>
2025-06-14 15:59:54 +0530

    Delete utils/pdf_utils.py

--- utils/pdf_utils.py
+++
@@ -1,9 +0,0 @@
-from PIL import Image
-import fitz
-import io
-
-def convert_pdf_first_page(pdf_bytes):
-    doc = fitz.open(stream=pdf_bytes, filetype="pdf")
-    page = doc.load_page(0)
-    pix = page.get_pixmap(dpi=300)
-    return Image.open(io.BytesIO(pix.tobytes("png")))
